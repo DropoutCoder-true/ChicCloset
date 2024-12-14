db1503a9b644b17b30806f3eb5eff0f90a1307d7
DropoutCoder-true <vighneshzagade4@gmail.com>
2024-12-14 08:53:45 +0000

    created navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
+import Checkout from "./scenes/checkout/Checkout";
+import { useEffect } from "react";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 function App() {
   return (
     <div className="app">
-      <h1>Initial Page</h1>
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/item/:itemId" element={<ItemDetails />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout/success" element={<Confirmation />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
