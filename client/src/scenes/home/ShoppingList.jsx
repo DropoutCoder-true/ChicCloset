946e6b05e23ee53bd02b17f70a0c0af09dc09e49
DropoutCoder-true <vighneshzagade4@gmail.com>
2024-12-16 04:35:47 +0000

    minor changes

---
+++ client/src/scenes/home/ShoppingList.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { Box, Typography, Tab, Tabs, useMediaQuery } from "@mui/material"
+import Item from "../../components/Item"
+import { setItems } from "../../state";
+
+
+const ShoppingList = () => {
+  return <div>ShoppingList</div>;
+};
+
+export default ShoppingList;
